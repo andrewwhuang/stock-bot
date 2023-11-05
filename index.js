d49796f004840c8c8239031a9f0ea14cb797852c
Andrew Huang <58315221+andrewwhuang@users.noreply.github.com>
2023-11-04 19:49:19 -0700

    .

--- index.js
+++ index.js
@@ -1,120 +1,123 @@
-const { DisTube } = require('distube')
-const Discord = require('discord.js')
-const client = new Discord.Client({
-  intents: [
-    Discord.GatewayIntentBits.Guilds,
-    Discord.GatewayIntentBits.GuildMessages,
-    Discord.GatewayIntentBits.GuildVoiceStates,
-    Discord.GatewayIntentBits.MessageContent
-  ]
-})
-const fs = require('fs')
-const config = require('./config.json')
-const { SpotifyPlugin } = require('@distube/spotify')
-const { SoundCloudPlugin } = require('@distube/soundcloud')
-const { YtDlpPlugin } = require('@distube/yt-dlp')
+import { downloadExecutable } from "./yt-dlp-utils";
+import { existsSync, readFileSync, writeFileSync, rmSync } from "node:fs";
+import { execSync } from "node:child_process";
+import { resolve } from "node:path";
+import { Server } from "node:http";
+import module from "node:module";
+import prism from "prism-media";
 
-client.config = require('./config.json')
-client.distube = new DisTube(client, {
-  leaveOnStop: false,
-  emitNewSongOnly: true,
-  emitAddSongWhenCreatingQueue: false,
-  emitAddListWhenCreatingQueue: false,
-  plugins: [
-    new SpotifyPlugin({
-      emitEventsAfterFetching: true
-    }),
-    new SoundCloudPlugin(),
-    new YtDlpPlugin()
-  ]
-})
-client.commands = new Discord.Collection()
-client.aliases = new Discord.Collection()
-client.emotes = config.emoji
+const ensureEnv = arr => arr.every(x => process.env[x] !== undefined);
 
-fs.readdir('./commands/', (err, files) => {
-  if (err) return console.log('Could not find any commands!')
-  const jsFiles = files.filter(f => f.split('.').pop() === 'js')
-  if (jsFiles.length <= 0) return console.log('Could not find any commands!')
-  jsFiles.forEach(file => {
-    const cmd = require(`./commands/${file}`)
-    console.log(`Loaded ${file}`)
-    client.commands.set(cmd.name, cmd)
-    if (cmd.aliases) cmd.aliases.forEach(alias => client.aliases.set(alias, cmd.name))
-  })
-})
+const isGlitch = ensureEnv([
+    "PROJECT_DOMAIN",
+    "PROJECT_INVITE_TOKEN",
+    "API_SERVER_EXTERNAL",
+    "PROJECT_REMIX_CHAIN"
+]);
 
-client.on('ready', () => {
-  console.log(`${client.user.tag} is ready to play music.`)
-})
+const isReplit = ensureEnv([
+    "REPLIT_DB_URL",
+    "REPL_ID",
+    "REPL_IMAGE",
+    "REPL_LANGUAGE",
+    "REPL_OWNER",
+    "REPL_PUBKEYS",
+    "REPL_SLUG"
+]);
 
-client.on('messageCreate', async message => {
-  if (message.author.bot || !message.guild) return
-  const prefix = config.prefix
-  if (!message.content.startsWith(prefix)) return
-  const args = message.content.slice(prefix.length).trim().split(/ +/g)
-  const command = args.shift().toLowerCase()
-  const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
-  if (!cmd) return
-  if (cmd.inVoiceChannel && !message.member.voice.channel) {
-    return message.channel.send(`${client.emotes.error} | You must be in a voice channel!`)
-  }
-  try {
-    cmd.run(client, message, args)
-  } catch (e) {
-    console.error(e)
-    message.channel.send(`${client.emotes.error} | Error: \`${e}\``)
-  }
-})
+const isGitHub = ensureEnv([
+    "GITHUB_ENV",
+    "GITHUB_REPOSITORY_OWNER",
+    "GITHUB_HEAD_REF",
+    "GITHUB_API_URL",
+    "GITHUB_REPOSITORY",
+    "GITHUB_SERVER_URL"
+]);
 
-const status = queue =>
-  `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.names.join(', ') || 'Off'}\` | Loop: \`${
-    queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'
-  }\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``
-client.distube
-  .on('playSong', (queue, song) =>
-    queue.textChannel.send(
-      `${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${
-        song.user
-      }\n${status(queue)}`
-    )
-  )
-  .on('addSong', (queue, song) =>
-    queue.textChannel.send(
-      `${client.emotes.success} | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
-    )
-  )
-  .on('addList', (queue, playlist) =>
-    queue.textChannel.send(
-      `${client.emotes.success} | Added \`${playlist.name}\` playlist (${
-        playlist.songs.length
-      } songs) to queue\n${status(queue)}`
-    )
-  )
-  .on('error', (channel, e) => {
-    if (channel) channel.send(`${client.emotes.error} | An error encountered: ${e.toString().slice(0, 1974)}`)
-    else console.error(e)
-  })
-  .on('empty', channel => channel.send('Voice channel is empty! Leaving the channel...'))
-  .on('searchNoResult', (message, query) =>
-    message.channel.send(`${client.emotes.error} | No result found for \`${query}\`!`)
-  )
-  .on('finish', queue => queue.textChannel.send('Finished!'))
-// // DisTubeOptions.searchSongs = true
-// .on("searchResult", (message, result) => {
-//     let i = 0
-//     message.channel.send(
-//         `**Choose an option from below**\n${result
-//             .map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``)
-//             .join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`
-//     )
-// })
-// .on("searchCancel", message => message.channel.send(`${client.emotes.error} | Searching canceled`))
-// .on("searchInvalidAnswer", message =>
-//     message.channel.send(
-//         `${client.emotes.error} | Invalid answer! You have to enter the number in the range of the results`
-//     )
-// )
-// .on("searchDone", () => {})
+function npmInstall(deleteDir = false, forceInstall = false, additionalArgs = []) {
+    if (deleteDir) {
+        const modulesPath = resolve(process.cwd(), "node_modules");
 
-client.login(config.token)
+        if (existsSync(modulesPath)) {
+            rmSync(modulesPath, {
+                recursive: true,
+                force: true
+            });
+        }
+    }
+
+    execSync(`npm install${isGlitch ? " --only=prod" : ""}${forceInstall ? " --force" : ""} ${additionalArgs.join(" ")}`);
+}
+
+if (isGlitch) {
+    const gitIgnorePath = resolve(process.cwd(), ".gitignore");
+    try {
+        const data = readFileSync(gitIgnorePath, "utf8").toString();
+        if (data.includes("dev.env")) {
+            writeFileSync(gitIgnorePath, data.replace("\ndev.env", ""));
+            console.info("Removed dev.env from .gitignore");
+        }
+    } catch {
+        console.error("Failed to remove dev.env from .gitignore");
+    }
+
+    try {
+        console.info("[INFO] Trying to re-install modules...");
+        npmInstall();
+        console.info("[INFO] Modules successfully re-installed.");
+    } catch (err) {
+        console.info("[INFO] Failed to re-install modules, trying to delete node_modules and re-install...");
+        try {
+            npmInstall(true);
+            console.info("[INFO] Modules successfully re-installed.");
+        } catch {
+            console.info("[INFO] Failed to re-install modules, trying to delete node_modules and install modules forcefully...");
+            try {
+                npmInstall(true, true);
+                console.info("[INFO] Modules successfully re-installed.");
+            } catch {
+                console.warn("[WARN] Failed to re-install modules, please re-install manually.");
+            }
+        }
+    }
+}
+
+if (isGitHub) {
+    console.warn("[WARN] Running this bot using GitHub is not recommended.");
+}
+
+const require = module.createRequire(import.meta.url);
+
+try {
+    prism.FFmpeg.getInfo(true);
+} catch {
+    console.info("[INFO] Couldn't find FFmpeg/avconv, trying to install ffmpeg-static...");
+    npmInstall(false, false, ["--no-save", "ffmpeg-static"]);
+    console.info("[INFO] ffmpeg-static has been installed.");
+}
+
+if (isGlitch || isReplit) {
+    new Server((req, res) => {
+        const now = new Date().toLocaleString("en-US");
+        res.end(`OK (200) - ${now}`);
+    }).listen(Number(process.env.PORT || 3000) || 3000);
+
+    console.info(`[INFO] ${isGlitch ? "Glitch" : "Replit"} environment detected, trying to compile...`);
+    execSync(`npm run compile`);
+    console.info("[INFO] Compiled.");
+}
+
+const streamStrategy = process.env.STREAM_STRATEGY;
+if (streamStrategy !== "play-dl") await downloadExecutable();
+if (streamStrategy === "play-dl") {
+    try {
+        require("play-dl");
+    } catch {
+        console.info("[INFO] Installing play-dl...");
+        npmInstall(false, false, ["play-dl"]);
+        console.info("[INFO] Play-dl has been installed.");
+    }
+}
+console.info("[INFO] Starting the bot...");
+
+import("./dist/index.js");
